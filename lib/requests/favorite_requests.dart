41f2b5cb69047f4ca9f0be4bd0e0c89d1092f7bc
TavernierAlicia <tavernieralicia00@gmail.com>
2020-05-26 12:58:07 +0200

    Implementing add / remove favorites in bar details view

--- lib/requests/favorite_requests.dart
+++ lib/requests/favorite_requests.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:projet_b3/model/bar.dart';
+import 'package:projet_b3/model/bar_info.dart';
 import 'package:projet_b3/model/favorite.dart';
 import 'package:projet_b3/requests/utils.dart';
 import 'package:http/http.dart';
@@ -17,13 +17,28 @@ Future<List<Favorite>>    getFavorites() async {
   return favorites ;
 }
 
-Future<String>        addToFavorites(Bar bar) async {
-  String    url = BASE_URL + "favs/add/" + bar.id.toString() ;
+Future<String>        addToFavorites(BarInfo barInfo) async {
+  String    url = BASE_URL + "favs/add/" + barInfo.barDetails.id.toString() ;
   Map<String, String> headers = {
     "Authorization" : "dcdb199e-2797-4041-8b26-08bc451dd47b"
   } ;
 
   Response    response = await post(url, headers: headers);
-  return response.body ;
-  // TODO : Handle fails
+
+  return jsonDecode(response.body) ;
+}
+
+Future<String>        removeFromFavorites(BarInfo barInfo) async {
+  String url = BASE_URL + "favs/delete/" + barInfo.barDetails.id.toString() ;
+  print("URL = $url");
+  Map<String, String> headers = {
+    "Authorization" : "dcdb199e-2797-4041-8b26-08bc451dd47b"
+  } ;
+
+  Response    response = await delete(url, headers: headers);
+
+  print("STATUS CODE = ${response.statusCode}");
+  print("DELETE FAV RESPONSE = ${response.body}");
+  print("decoded = ${jsonDecode(response.body)}");
+  return jsonDecode(response.body) ;
 }
