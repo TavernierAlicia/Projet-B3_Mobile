67bb84364d823afca3b160c4db12e3e8cb9048d0
TavernierAlicia <tavernieralicia00@gmail.com>
2020-05-27 08:18:05 +0200

    Adding splashscreen, disabling landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:projet_b3/pages/page_login.dart';
 import 'package:projet_b3/pages/page_main.dart';
 import 'package:projet_b3/user_data.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+      ],
+    );
+    
+    return new SplashScreen(
+      seconds: 15,  // TODO
+      navigateAfterSeconds: (isUserLoggedIn) ? MainPage() : PageLogin(),
+      backgroundColor: Colors.deepOrange,
+      styleTextUnderTheLoader: new TextStyle(),
+      imageBackground: Image.asset("assets/logo.png").image,
+      loaderColor: Colors.white,
+    );
     return MaterialApp(
       title: 'Projet B3',
       theme: ThemeData(
@@ -19,6 +47,25 @@ class MyApp extends StatelessWidget {
         "page_login" : (context) => PageLogin(),
         "page_main" : (context) => MainPage(),
       },
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Welcome In SplashScreen Package"),
+        automaticallyImplyLeading: false,
+      ),
+      body: new Center(
+        child: new Text(
+          "Succeeded!",
+          style: new TextStyle(fontWeight: FontWeight.bold,
+              fontSize: 30.0),
+        ),
+      ),
     );
   }
 }
