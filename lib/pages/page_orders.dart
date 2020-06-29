63822a3dffa45e5b37d97cbaf8978eb8c0efec9a
TavernierAlicia <tavernieralicia00@gmail.com>
2020-06-29 20:19:56 +0200

    Adding no order yet layout

--- lib/pages/page_orders.dart
+++ lib/pages/page_orders.dart
@@ -47,9 +47,9 @@ class _PageOrdersState extends State<PageOrders> {
     return FutureBuilder(
       future: _ordersList,
       builder: (context, snapshot) {
-        return (snapshot.data != null) ? SingleChildScrollView(
-          child: _orders(snapshot.data as List<Order>),
-        ) : Center(
+        return (snapshot.data != null)
+            ? _orders(snapshot.data as List<Order>)
+            : Center(
           child: SizedBox(
             width: 100,
             height: 100,
@@ -61,14 +61,25 @@ class _PageOrdersState extends State<PageOrders> {
   }
 
   Widget _orders(List<Order> orders) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: orders.length,
-      itemBuilder: (context, i) {
-        return orderItem(context, orders[i], orderAgain: _orderAgain,
-          orderClicked: _orderClicked,);
-      },
+
+    if (orders.length == 0) {
+      return Center(
+        child: Text(
+            "Aucune commande pour l'instant !"
+        ),
+      );
+    }
+
+    return SingleChildScrollView(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: orders.length,
+        itemBuilder: (context, i) {
+          return orderItem(context, orders[i], orderAgain: _orderAgain,
+            orderClicked: _orderClicked,);
+        },
+      ),
     );
   }
 
